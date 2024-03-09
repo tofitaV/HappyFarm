d92904927fe801d9caf62a4be5f853fb88983f8d
tofita <tofita1337@gmail.com>
2024-03-09 16:25:21 +0200

    fix buttons

--- src/FloatingButton.tsx
+++ src/FloatingButton.tsx
@@ -12,6 +12,8 @@ const FloatingButton: React.FC<Props> = ({handleElementSelection}) => {
             bottom: '20px',
             left: '50%',
             transform: 'translateX(-50%)',
+            width: '100%',
+            textAlign: 'center'
         }}>
             <button onClick={() => handleElementSelection('corn')}>
                 Corn 🌽
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import GameField from "./GameField";
-import FloatingButton from "./FloatingButton";
 import GlobalWindow from "./GlobalWindows";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
