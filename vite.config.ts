8b2b3d01c74aa4943727103d22b10b339b771aa4
tofita <tofita1337@gmail.com>
2024-03-09 16:15:22 +0200

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/happyfarmer/",
+  base: "./",
   server: {
     port: 443,
     host: "0.0.0.0",
     hmr: {
-      host: 'HappyFarm.local',
+      host: 'happyfarmer.local',
       port: 443,
     },
   }
