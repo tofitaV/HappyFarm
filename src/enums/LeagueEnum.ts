76e54f89765d8cf61f664c10c5a90c03a8131c4b
tofita1337 <tofita1337@gmail.com>
2024-04-13 23:04:22 +0300

    fix assets

--- src/enums/LeagueEnum.ts
+++ src/enums/LeagueEnum.ts
@@ -2,21 +2,21 @@ export const leagues = [
     {
         name: "Bronze",
         id: 0,
-        image: "public/assets/league/bronze.png"
+        image: "/assets/league/bronze.png"
     },
     {
         name: "Silver",
         id: 1,
-        image: "public/assets/league/silver.png"
+        image: "/assets/league/silver.png"
     },
     {
         name: "Gold",
         id: 2,
-        image: "public/assets/league/gold.png"
+        image: "/assets/league/gold.png"
     },
     {
         name: "Platinum",
         id: 3,
-        image: "public/assets/league/platinum.png"
+        image: "/assets/league/platinum.png"
     }
 ];
--- src/enums/PlantsEnum.ts
+++ src/enums/PlantsEnum.ts
@@ -4,25 +4,25 @@ export const plants = [
     {
         name: "Corn",
         id: 0,
-        image: "public/assets/plants/corn.png",
+        image: "/assets/plants/corn.png",
         type: PlantEnum.Corn
     },
     {
         name: "Carrot",
         id: 1,
-        image: "public/assets/plants/carrot.png",
+        image: "/assets/plants/carrot.png",
         type: PlantEnum.Carrot
     },
     {
         name: "Pepper",
         id: 2,
-        image: "public/assets/plants/pepper.png",
+        image: "/assets/plants/pepper.png",
         type: PlantEnum.Pepper
     },
     {
         name: "Seed",
         id: 3,
-        image: "public/assets/plants/seed.png",
+        image: "/assets/plants/seed.png",
         type: PlantEnum.Seed
     }
 ];
