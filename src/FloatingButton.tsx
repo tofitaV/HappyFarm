a84d66226b96227f776f4e212ddb5ee9b4400b2b
tofita1337 <tofita1337@gmail.com>
2024-07-20 13:54:57 +0300

    change nav menu and action buttons

--- src/FloatingButton.tsx
+++ src/FloatingButton.tsx
@@ -1,35 +1,29 @@
-import React, {useContext, useState} from 'react';
+import React, { useContext, useState } from 'react';
 import './FloatingButton.scss';
-import {initCarrot} from './Plant/Type/Carrot';
-import {initPepper} from './Plant/Type/Pepper';
-import {MyContext} from './contexts/AppContext';
-import {DigUpAction} from './Actions/DigUpAction';
-import {HarvestAction} from './Actions/HarvestAction';
-import {ToWaterPlantAction} from './Actions/ToWaterPlantAction';
-import {PlantEnum} from './Plant/PlantEnum';
-import {initCorn} from './Plant/Type/Corn';
-import {initNothing} from './Plant/Type/Nothing';
-import {DoNothing} from './Actions/DoNothing';
-import {FairModal} from "./Fair";
-import {PlantStoreModal} from "./PlantStoreModal";
-import {League} from "./League/League";
-import SpinWheel from "./DailyActivity/SpinWheel";
-import {WalletModal} from "./wallet/WalletModal";
-import FriendsList from "./Friends/FriendsList";
-
+import { initCarrot } from './Plant/Type/Carrot';
+import { initPepper } from './Plant/Type/Pepper';
+import { MyContext } from './contexts/AppContext';
+import { DigUpAction } from './Actions/DigUpAction';
+import { HarvestAction } from './Actions/HarvestAction';
+import { ToWaterPlantAction } from './Actions/ToWaterPlantAction';
+import { PlantEnum } from './Plant/PlantEnum';
+import { initCorn } from './Plant/Type/Corn';
+import { initNothing } from './Plant/Type/Nothing';
+import { DoNothing } from './Actions/DoNothing';
+import { FairModal } from './Fair';
+import { PlantStoreModal } from './PlantStoreModal';
+import { League } from './League/League';
+import SpinWheel from './DailyActivity/SpinWheel';
+import { WalletModal } from './wallet/WalletModal';
+import FriendsList from './Friends/FriendsList';
 
 const FloatingButton: React.FC = () => {
-    const [showSecondRow, setShowSecondRow] = useState(false);
     const [showPlantStoreModal, setShowPlantStoreModal] = useState(false);
     const [showFairModal, setShowFairModal] = useState(false);
     const [showLeagueModal, setShowLeagueModal] = useState(false);
     const [showFriendsModal, setShowFriendsModal] = useState(false);
 
-    const {action, setAction, plant, setPlant} = useContext(MyContext);
-
-    const toggleSecondRow = () => {
-        setShowSecondRow(!showSecondRow);
-    };
+    const { action, setAction, plant, setPlant } = useContext(MyContext);
 
     const togglePlantStore = () => {
         setShowPlantStoreModal(!showPlantStoreModal);
@@ -42,6 +36,7 @@ const FloatingButton: React.FC = () => {
     const toggleLeague = () => {
         setShowLeagueModal(!showLeagueModal);
     };
+
     const toggleFriendsList = () => {
         setShowFriendsModal(!showFriendsModal);
     };
@@ -59,71 +54,85 @@ const FloatingButton: React.FC = () => {
     };
 
     return (
-
         <div>
-            <div className="action-buttons">
-                {showSecondRow && (
-                    <div style={{marginTop: '1px'}}>
-                        <button onClick={() => {
+            <div className="action-buttons-container">
+                <div className="action-buttons">
+                    <button
+                        onClick={() => {
                             setAction(new ToWaterPlantAction());
                             setPlant(initNothing);
-                        }} className={action instanceof ToWaterPlantAction ? 'active' : ''}>
-                            💧
-                        </button>
-                        <button onClick={() => {
+                        }}
+                        className={`action-button ${action instanceof ToWaterPlantAction ? 'active' : ''}`}
+                    >
+                        💧
+                    </button>
+                    <button
+                        onClick={() => {
                             setAction(new DigUpAction());
                             setPlant(initNothing);
-                        }} className={action instanceof DigUpAction ? 'active' : ''}>
-                            Dig up
-                        </button>
-                        <button onClick={() => {
+                        }}
+                        className={`action-button ${action instanceof DigUpAction ? 'active' : ''}`}
+                    >
+                        🏃
+                    </button>
+                    <button
+                        onClick={() => {
                             setAction(new HarvestAction());
                             setPlant(initNothing);
-                        }} className={action instanceof HarvestAction ? 'active' : ''}>
-                            Harvest
-                        </button>
-                    </div>
-                )}
-                <button onClick={togglePlantStore}>
-                    {'Store'}
+                        }}
+                        className={`action-button ${action instanceof HarvestAction ? 'active' : ''}`}
+                    >
+                        🌾
+                    </button>
+                    <button
+                        onClick={() => {
+                            setAction(new DoNothing());
+                            setPlant(initNothing);
+                        }}
+                        className="action-button"
+                    >
+                        ❌
+                    </button>
+                </div>
+            </div>
+            <div className="navigation-menu">
+                <button
+                    onClick={togglePlantStore}
+                    className="navigation-button"
+                >
+                    🛒
+                    <div className="navigation-label">Store</div>
                 </button>
-                <button onClick={toggleFair}>
-                    {'Fair'}
+                <button
+                    onClick={toggleFair}
+                    className="navigation-button"
+                >
+                    🎪
+                    <div className="navigation-label">Fair</div>
                 </button>
-                <button onClick={toggleLeague}>
-                    {'League'}
+                <button
+                    onClick={toggleLeague}
+                    className="navigation-button"
+                >
+                    🏆
+                    <div className="navigation-label">League</div>
                 </button>
-                <button onClick={toggleFriendsList}>
-                    {'Friends'}
-                </button>
-                <button onClick={toggleSecondRow}>
-                    {showSecondRow ? 'Action' : 'Action'}
-                </button>
-                <button onClick={() => {
-                    setAction(new DoNothing());
-                    setPlant(initNothing);
-                }}>
-                    ❌
+                <button
+                    onClick={toggleFriendsList}
+                    className="navigation-button"
+                >
+                    👥
+                    <div className="navigation-label">Friends</div>
                 </button>
             </div>
-            {/* Modal should be rendered here, outside the button wrapper */}
             <PlantStoreModal
                 show={showPlantStoreModal}
                 onClose={togglePlantStore}
                 onSelectPlant={handleSelectPlant}
             />
-            <FairModal
-                show={showFairModal}
-                onClose={toggleFair}
-            />
-            <League
-                show={showLeagueModal}
-                onClose={toggleLeague}
-            />
-            <FriendsList
-                show={showFriendsModal}
-                onClose={toggleFriendsList}
-            />
+            <FairModal show={showFairModal} onClose={toggleFair} />
+            <League show={showLeagueModal} onClose={toggleLeague} />
+            <FriendsList show={showFriendsModal} onClose={toggleFriendsList} />
         </div>
     );
 };
